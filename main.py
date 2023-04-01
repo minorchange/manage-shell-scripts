3910d46bc0fe52105329ed1594448a9740a8d3ac
minorchange <bnu@posteo.org>
2023-04-01 13:26:30 +0200

    Refactor main.py
    Increase readability by introducing some small funcions.

--- main.py
+++ main.py
@@ -6,23 +6,30 @@ import argparse
 from pathlib import Path
 
 
-if __name__ == "__main__":
+def folderpath_from_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument("path")
     args = parser.parse_args()
-    path = Path(args.path)
-    
+    return Path(args.path)
+
+
+def scripfilepaths_from_folder(path):
     scripts = [
         join(path, f)
         for f in listdir(path)
         if isfile(join(path, f)) and f.endswith(".sh")
     ]
-
     scripts.sort()
-    n_scripts = len(scripts)
-    logger.info(f"    Starting execute scripts in {path}")
+    return scripts
+
+
+if __name__ == "__main__":
+    folder_path = folderpath_from_arg()
+    scripts = scripfilepaths_from_folder(folder_path)
+
+    logger.info(f"Starting execute scripts in {folder_path}")
     for i, s in enumerate(scripts):
-        logger.debug(f"    {i + 1}/{n_scripts} - Starting to run script: {s}")
+        logger.debug(f"    {i + 1}/{len(scripts)} - Starting to run script: {s}")
 
         a = subprocess.run(["sh", s], capture_output=True)
 
@@ -34,4 +41,4 @@ if __name__ == "__main__":
             logger.error(
                 f"Script {s} failed!\n    The output was:\n    {a.stdout}\n    The error was:\n    {a.stderr}"
             )
-    logger.info(f"    Finished execute scripts in {path}")
+    logger.info(f"Finished execute scripts in {folder_path}")
